c495d344adab806437759b16146e015c4f8a1c8d
Maycon Willian <mayconwas2018@gmail.com>
2020-03-14 15:52:50 -0300

    criando e menu e sub menu para o cadastro de produtos

--- ex_produto.cpp
+++ ex_produto.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct produto_t {
 	
-        char nome[50];
+	char nome[50];
 	int codigo, qtd;
 	double preco;
 	
@@ -19,9 +19,7 @@ static list<produto_t> lista_produtos;
 
 void adicionar_produto() {
 		
-              
-        produto_t produto;       	
-	
+	produto_t produto;	
 
 	printf("\nDigite o nome do produto: ");
 	scanf("%s", &produto.nome);
@@ -33,32 +31,86 @@ void adicionar_produto() {
 	scanf("%i", &produto.qtd);
 
 	printf("\nDigite o preço do produto:");
-	scanf("%1f", &produto.preco);
+	scanf("%lf", &produto.preco);
 
-        lista_produtos.push_back(produto);	
-       
+    lista_produtos.push_back(produto);       
 
 };
 
 void listar_produto() {
 
+	printf("----- lISTA DE PRODUTOS -----\n");
+
     for (list<produto_t>::iterator listar = lista_produtos.begin(); listar != lista_produtos.end(); listar++){
         cout << "Produto: " << (listar)->nome << "\nCódigo: " << (listar)->codigo << "\nQuantidade: "
              << (listar)->qtd << "\nPreço: " << (listar)->preco << endl;
+		printf("--------------------------\n");
         
-    };
+    }
 
-};     
+};
+
+int menu_principal() {
+
+	int opcao;
 	
+	while(opcao != 3) {
+		
+		printf("=== Escolha Uma das opções do Menu: ===\n");
+		printf("\n1 - Adcionar Produtos: \n");
+		printf("2 - Listar: \n");
+		printf("3 - Voltar: \n");
+		printf("\n====================================\n");
 
+		printf("\nQual opção você deseja: ");
+		scanf("%i", &opcao);
+		
+		if(opcao == 1) {
+
+			adicionar_produto();					
+
+		} else if (opcao == 2) {
+
+			listar_produto();
+		
+		} else if (opcao == 3){
+			
+			printf("\n----- Voltando para a Tela Inicial -----");	
+			return 0;
+		}	
+
+	}
+	
+};
 
 int main()
 {	
+	char resposta[5];
+	int i;
 
-	adicionar_produto();
-        listar_produto();
+	printf ("================================");
+	printf ("\n=== BEM VINDO AO SIS-ESTOQUE ===\n");
+	printf ("================================\n");
+	printf ("\nDeseja cadastrar um produto? Informe: ");	
+    scanf ("%s",&resposta);
+	
+	while(1) {
+
+    	if (!strcmp(resposta, "sim")) {
+    				
+			menu_principal();             
+        
+		} else {
+
+			printf("\n--- Fechando o Sistema ---\n");
+			printf("\n--- Obrigado! ---\n");
+			break;
+			
+		}
+	}			
         
 
 	return 0;
 
-}
+};
+
